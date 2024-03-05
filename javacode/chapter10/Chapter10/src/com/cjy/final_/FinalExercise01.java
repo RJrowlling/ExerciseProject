32657f93ea66104d057391b686b5687f8e8a2b90
cjy <cjy2442972980@163.com>
2024-03-05 21:09:46 +0800

    0基础学java第十天

---
+++ javacode/chapter10/Chapter10/src/com/cjy/final_/FinalExercise01.java
@@ -0,0 +1,23 @@
+package com.cjy.final_;
+
+public class FinalExercise01 {
+
+}
+
+class Area {
+    public final double PI = 3.14;
+
+    public final double PI_1;
+
+    public final double PI_2;
+
+    public final static double PI_3 = 3.14;
+
+    public Area() {
+        PI_1 = 3.14;
+    }
+
+    {
+        PI_2 = 3.14;
+    }
+}
