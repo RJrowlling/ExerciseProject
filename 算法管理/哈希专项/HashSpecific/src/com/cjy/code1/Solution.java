45afc08caff9d0a1673fe5a5ee33a8340ff81652
cjy <cjy2442972980@163.com>
2024-03-23 12:07:59 +0800

    更新README文档

--- 算法管理/哈希专项/HashSpecific/src/com/cjy/code1/Solution.java
+++ 算法管理/哈希专项/HashSpecific/src/com/cjy/code1/Solution.java
@@ -1,5 +1,7 @@
 package com.cjy.code1;
 
+import java.util.Optional;
+
 /**
  * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
  * 若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
@@ -9,6 +11,7 @@ package com.cjy.code1;
  */
 public class Solution {
     public static void main(String[] args) {
+        Optional
         String s = "rat";
         String t = "car";
         System.out.println(isAnagram(s, t));
