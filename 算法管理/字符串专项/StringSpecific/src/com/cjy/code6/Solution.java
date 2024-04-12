d78e35480d7e10a9ab98f09b8e30455eb4c1efd5
cjy <cjy2442972980@163.com>
2024-04-12 20:04:09 +0800

    算法更新

--- 算法管理/字符串专项/StringSpecific/src/com/cjy/code6/Solution.java
+++ 算法管理/字符串专项/StringSpecific/src/com/cjy/code6/Solution.java
@@ -7,7 +7,28 @@ package com.cjy.code6;
  * @Description: 给定一个非空的字符串 s ，检查是否可以通过由它的一个子串重复多次构成。
  */
 public class Solution {
-    public boolean repeatedSubstringPattern(String s) {
+    public static void main(String[] args) {
+        String s = "aba";
+        System.out.println(repeatedSubstringPattern(s));
+    }
+    public static boolean repeatedSubstringPattern(String s) {
+        label1:
+        for (int i = 0; i < s.length() / 2; i++) {
+            String subString = s.substring(0, i + 1);
+            int subLength = i + 1;
 
+            if (s.length() % subLength != 0) {
+                continue;
+            }
+
+            for (int j = 1; j < s.length() / subLength; j++) {
+                String tempString = s.substring((i + 1) * j, (i + 1) * (j + 1));
+                if (!subString.equals(tempString)) {
+                   continue label1;
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
