15ac4f18c0e1f0d34acb9fd50964091575b51908
cjy <cjy2442972980@163.com>
2024-06-04 20:12:10 +0800

    mysql学习第6天

---
+++ 算法管理/二叉树/BinaryTreeSpecific/src/com/cjy/demo27/Solution.java
@@ -0,0 +1,51 @@
+package com.cjy.demo27;
+
+/**
+ * @Author cjy
+ * @Date 2024/6/1 18:20
+ * @Version 1.0
+ * @Description:
+ */
+public class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        }
+
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+
+
+
+
+
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
